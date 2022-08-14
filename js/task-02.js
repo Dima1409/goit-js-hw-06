f6b6229d212328aced10feadcd7f4d67cbe51958
Dima1409 <57713733+Dima1409@users.noreply.github.com>
2022-08-15 00:35:09 +0300

    task3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulIngredients = document.querySelector("#ingredients");
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const newElement = document.createElement("li");
+  newElement.classList.add("item");
+  newElement.textContent = ingredients[i];
+  console.log(newElement);
+  // ulIngredients.appendChild(newElement);
+  ulIngredients.append(newElement);
+}
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,31 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const galleryList = document.querySelector(".gallery");
+galleryList.style.cssText = `
+display: flex;
+list-style: none;
+align-items: center;
+justify-content: space-between;
+padding:0;
+margin:0;
+`;
+
+for (const element of images) {
+  galleryList.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery-item"><img style='display:block; border-radius:10px' class='item-image' src="${element.url}" alt="${element.alt}" width="400" '</li>`
+  );
+}
