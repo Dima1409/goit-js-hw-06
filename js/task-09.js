84c778fc4a486dee5f22e3b92de7470369632626
Dima <57713733+Dima1409@users.noreply.github.com>
2022-08-19 11:04:49 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+
+const changeColorButton = document.querySelector('.change-color')
+
+const colorName = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', handleChangeColor);
+
+function handleChangeColor() {
+  body.style.backgroundColor=getRandomHexColor();
+  colorName.textContent = getRandomHexColor()
+}
