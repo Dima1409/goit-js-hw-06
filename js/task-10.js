8fa0cd249485321ff97c38a167d3682aecb10a56
Dima <57713733+Dima1409@users.noreply.github.com>
2022-08-19 18:00:47 +0300

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,55 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const ref = {
+  input: document.querySelector('#controls>input'), // INPUT
+  destroy: document.querySelector('[data-destroy]'), // DESTROY BTN
+  create: document.querySelector('[data-create]'), // CREATE BTN
+  boxes: document.querySelector('#boxes'), // BOXES DIV
+}
+/////////////////////////////////////////////////////////////
+ref.input.addEventListener('change', handleInputValue);
+  
+  function handleInputValue() {
+    return Number(ref.input.value);
+}
+const userValue = handleInputValue;
+//////////////////////////////////////////////////////////////
+ref.create.addEventListener('click', createElementToHtml);
+
+
+function createElementToHtml() {
+    const dimensions = {
+      width: 30,
+      height: 30,
+      nextDivDimensions: 0,
+    }
+    const elements =[];
+    for(let i = 0; i<=userValue(); i+=1) { 
+      dimensions.nextDivDimensions+=10;
+      const a=document.createElement('div'); 
+      a.style.backgroundColor = getRandomHexColor();
+      a.style.width = dimensions.width + dimensions.nextDivDimensions +'px'
+      a.style.height= dimensions.height + dimensions.nextDivDimensions + 'px'
+      
+      elements.push(a);
+  
+    }
+    boxes.append(...elements);
+    
+}
+
+
+
+
+
+
+
+// функція для очищення всього списку елементів
+ref.destroy.addEventListener('click', handleDestroy);
+function handleDestroy() {
+  ref.boxes.innerHTML=''
+  ref.input.value = 0
+
+}
